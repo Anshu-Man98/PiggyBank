d731a1062b085f71077ef25a1db72ca5a3fc033f
Anshu-Man <amnshuman98@gmail.com>
2022-06-22 15:25:13 +0530

    FileNameUpdated

---
+++ PiggyBank/View/ExpenceListViewPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PiggyBank.View
+{
+    public partial class ExpenceListViewPage : ContentPage
+    {
+        public ExpenceListViewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
